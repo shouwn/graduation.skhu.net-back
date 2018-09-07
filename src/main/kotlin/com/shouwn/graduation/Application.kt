e830690267cf7560d233d074556248a97683e919
shouwn <sjonghyon@gmail.com>
2018-09-07 18:26:21 +0900

    프로퍼티 설정

--- src/main/kotlin/com/shouwn/graduation/Application.kt
+++ src/main/kotlin/com/shouwn/graduation/Application.kt
@@ -2,10 +2,18 @@ package com.shouwn.graduation
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import java.util.HashMap
+
 
 @SpringBootApplication
 class Application
 
 fun main(args: Array<String>) {
-    runApplication<Application>(*args)
+    runApplication<Application>(*args){
+        setDefaultProperties(
+                hashMapOf("spring.config.location"
+                        to "classpath:/application.yml, /app/build/config/graduation/application.yml")
+                        as Map<String, Any>)
+    }
 }
--- src/main/kotlin/com/shouwn/graduation/controller/TestController.kt
+++ src/main/kotlin/com/shouwn/graduation/controller/TestController.kt
@@ -1,15 +1,25 @@
 package com.shouwn.graduation.controller
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("test")
-class TestController {
+class TestController(
+        val env: Environment
+) {
 
     @GetMapping("hello")
-    fun hello() : String {
+    fun hello(): String {
         return "hello"
     }
+
+    @GetMapping("profile")
+    fun profile(): String? {
+        return env.activeProfiles.firstOrNull()
+    }
+
 }
