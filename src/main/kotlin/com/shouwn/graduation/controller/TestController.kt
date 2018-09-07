752a4aff32cb039f582e1ee77336671f5b9151ff
shouwn <sjonghyon@gmail.com>
2018-09-07 15:49:33 +0900

    코틀린으로 변경

---
+++ src/main/kotlin/com/shouwn/graduation/controller/TestController.kt
@@ -0,0 +1,15 @@
+package com.shouwn.graduation.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("test")
+class TestController {
+
+    @GetMapping("hello")
+    fun hello() : String {
+        return "hello"
+    }
+}
