3b5d72cab6d10615eabbb30e860960feb569b9a3
shouwn <sjonghyon@gmail.com>
2018-10-22 23:03:33 +0900

    힌트 수정

--- src/main/kotlin/com/shouwn/graduation/model/domain/Hint.kt
+++ src/main/kotlin/com/shouwn/graduation/model/domain/Hint.kt
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.annotation.JsonFormat
 
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 enum class Hint(val value: Long, val label: String){
-    H1(0, "가장 소중한 보물"),
-    H2(1, "어머니 이름"),
-    H3(2, "아버지 이름"),
-    H4(3, "태어난 장소")
+    H1(0, "가장 기억에 남는 장소는?"),
+    H2(1, "나의 보물 제1호는?"),
+    H3(2, "나의 출신 초등학교는?"),
+    H4(3, "좋아하는 스포츠 팀 이름은?")
 }
