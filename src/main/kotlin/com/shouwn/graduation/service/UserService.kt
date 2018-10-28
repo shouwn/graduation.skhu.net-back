cd8540c4bf1e050d96fee6eed0dff77b3e32bfe5
shouwn <sjonghyon@gmail.com>
2018-10-29 00:02:00 +0900

    학생 정보 가져오는 API 구현

--- src/main/kotlin/com/shouwn/graduation/service/UserService.kt
+++ src/main/kotlin/com/shouwn/graduation/service/UserService.kt
@@ -3,17 +3,20 @@ package com.shouwn.graduation.service
 import com.shouwn.graduation.model.domain.dto.request.PasswordModifyRequest
 import com.shouwn.graduation.model.domain.dto.request.UserDataModifyRequest
 import com.shouwn.graduation.model.domain.dto.response.ApiResponse
+import com.shouwn.graduation.model.domain.dto.response.StudentReponse
 import com.shouwn.graduation.model.domain.entity.Attend
+import com.shouwn.graduation.model.domain.entity.User
 import com.shouwn.graduation.model.domain.exception.ApiException
 import com.shouwn.graduation.model.domain.type.BelongType
+import com.shouwn.graduation.model.domain.type.RoleName
 import com.shouwn.graduation.repository.UserRepository
 import com.shouwn.graduation.security.UserPrincipal
 import com.shouwn.graduation.utils.findAllById
 import com.shouwn.graduation.utils.info
 import com.shouwn.graduation.utils.logger
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
-import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
@@ -24,6 +27,18 @@ class UserService @Autowired constructor(
         private val partyService: PartyService,
         private val passwordEncoder: PasswordEncoder
 ){
+
+    @Value("\${year.first}")
+    private var firstYear: Long? = null
+
+    @Value("\${year.second}")
+    private var secondYear: Long? = null
+
+    @Value("\${year.second}")
+    private var thirdYear: Long? = null
+
+    @Value("\${year.second}")
+    private var fourthYear: Long? = null
 
     private val logger = logger()
 
@@ -60,5 +75,45 @@ class UserService @Autowired constructor(
     fun findUsersById(ids: Iterable<Long>) =
             findAllById(userRepository, ids)
 
+    fun student(user: User) =
+            user.apply {
+                if(user.role != RoleName.ROLE_STUDENT)
+                    throw ApiException(
+                            status = HttpStatus.PRECONDITION_FAILED,
+                            apiResponse = ApiResponse(
+                                    success = false,
+                                    message = "${user.id}는 학생이 아닙니다."
+                            )
+                    )
+            }.let {
+                StudentReponse(
+                        id = it.id!!,
+                        userNumber = it.userNumber,
+                        year = calStudentYearByAttends(it.attends!!),
+                        name = it.name,
+                        email = it.email,
+                        role = it.role,
+                        parties = it.parties!!
+                )
+            }
 
+    fun calStudentYearByAttends(attends: MutableSet<Attend>): Long {
+        val grade: Double = try{
+            attends.asSequence()
+                    .map { it.credit }
+                    .reduce { acc, d ->  acc + d }
+        } catch (e: UnsupportedOperationException){
+            0.0
+        }
+        if(grade < firstYear!!)
+            return 1
+
+        if(grade < secondYear!!)
+            return 2
+
+        if(grade < thirdYear!!)
+            return 3
+
+        return 4
+    }
 }
